549c4ab931692dc2bbfd88039ac73629f9d4f273
bhuminiecki <blahy998@gmail.com>
2019-01-11 18:21:15 +0100

    implemented json changes to side branch

--- src/startSimulation.js
+++ src/startSimulation.js
@@ -1,9 +1,17 @@
 var btn = document.getElementById("start");
 
-btn.onclick = function startSim(input) {
+function startSim(input) {
     $.ajax({
         type: "POST",
         url: "src/simulation.py",
         data: { param: input },
-    });
-}
+    }).done(function (o) {
+        console.log("done");
+        document.getElementById("graph").innerHTML;
+    })
+};
+
+btn.onclick = function() {
+    console.log("started");
+    startSim();
+};
