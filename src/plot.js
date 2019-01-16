2e7b00a7155c96c43e5ad8fef7af4ed9d46efcca
bhuminiecki <blahy998@gmail.com>
2019-01-16 19:16:50 +0100

    changed file structure included eel module

--- src/plot.js
+++ src/plot.js
@@ -5,8 +5,8 @@ Plotly.d3.csv('out/output.csv', function(err, rows){
         { return row[key]; }); }
 
     var x = unpack(rows , 'x');
-    var y = unpack(rows , 'y');
-    var z = unpack(rows , 'z');
+    var z = unpack(rows , 'y');
+    var y = unpack(rows , 'z');
 
     var c = unpack(rows , 'color');
 
@@ -32,6 +32,48 @@ Plotly.d3.csv('out/output.csv', function(err, rows){
         }
     };
 
+    var trace1 = {
+        type: 'scatter3d',
+        mode: 'markers',
+        x: x,
+        y: y,
+        z: z,
+        opacity: 1,
+
+        marker: {
+            size: 5,
+            color: c,
+            colorscale: "Reds",
+            cmin: -20,
+            cmax: 50
+        }
+    };
+
+    var frames = [];
+    for(i = 0; i<x.length; i++)
+    {
+        frames.push(
+            {
+                name: i,
+                data: [trace0,trace1[i]]
+            }
+        );
+    };
+
+    var sliderSteps = [];
+    for(i = 1; i<x.length; i++)
+    {
+        sliderSteps.push({
+            method: 'animate',
+            label: i,
+            args: [i, {
+                mode: 'immediate',
+                transition: {duration: 100},
+                frame: {duration: 100, redraw: false},
+            }]
+        });
+    };
+
     var layout = {
         height: 640,
         xaxis: {
@@ -42,9 +84,55 @@ Plotly.d3.csv('out/output.csv', function(err, rows){
         },
         zaxis:{
             range: [Math.min(Math.min(x),Math.min(y),Math.min(z)),Math.max(Math.max(x),Math.max(y),Math.max(z))]
-        }
+        },
+
+        updatemenus: [{
+            x: 0,
+            y: 0,
+            yanchor: 'top',
+            xanchor: 'left',
+            showactive: true,
+            direction: 'left',
+            type: 'buttons',
+            pad: {t: 87, r: 10},
+            buttons: [{
+                method: 'animate',
+                args: [null, {
+                    mode: 'immediate',
+                    fromcurrent: true,
+                    transition: {duration: 300},
+                    frame: {duration: 500, redraw: false}
+                }],
+                label: 'Play'
+            }, {
+                method: 'animate',
+                args: [[null], {
+                    mode: 'immediate',
+                    transition: {duration: 0},
+                    frame: {duration: 0, redraw: false}
+                }],
+                label: 'Pause'
+            }]
+        }],
+        // Finally, add the slider and use `pad` to position it
+        // nicely next to the buttons.
+        sliders: [{
+            pad: {l: 130, t: 55},
+            currentvalue: {
+                visible: true,
+                prefix: 'Step:',
+                xanchor: 'right',
+                font: {size: 20, color: '#666'}
+            },
+            steps: sliderSteps
+        }]
     };
 
-    Plotly.plot('graph', [trace0], layout);
+    Plotly.plot('graph',{
+        data: [trace0,trace1],
+        layout: layout,
+    }).then(function(){
+        Plotly.addFrames('graph',frames)
+    });
 });
 
---
+++ src/saveConfig.js
@@ -0,0 +1,61 @@
+var btn = document.getElementById("save");
+
+function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
+
+var data = {
+    simulation : {
+        time-step : 0.010,
+        steer-step : 1,
+        save-step : 15,
+        humidity : 0.6,
+        temperature : 293,
+        pressure : 101325,
+        wind : [0, 0, 1],
+        counter_velocity : true
+    },
+
+    rocket : {
+        flight_control : {
+            altitude : null,
+            proportional_regulation : 1,
+            differential_regulation : 1,
+            max_thrust_angle : 0.10,
+            react_angle : 0.01,
+            start_steer : 1,
+            start_dive : 0
+        },
+        mass : {
+            total : 1,
+            change : -0.001,
+            fuel : 0.5
+        },
+        direction : {
+            angle: null,
+            xyz : [ 0.0, 1.0, 0.0 ]
+        },
+        drag_coefficient : {
+            front : 0.05,
+            side : 0.15},
+        surface : {
+            side : 1.0,
+            front : 0.1
+        },
+        thrust : {
+            f0 : 100,
+            change : 1
+        }
+    }
+};
+
+btn.onclick = function()
+{
+    console.log("Reeeee");
+    download(JSON.stringify(data), 'res/input.json', 'text/plain');
+    console.log("Raaaaaa");
+};
